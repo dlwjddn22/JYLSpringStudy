17acd43b9004ef907907336854f3ab5c35d082df
JF <JF@JF-PC>
2015-07-12 15:58:44 +0900

    마이바티스 설정(미완성)

---
+++ jylSystem/src/main/java/sample/service/SampleServiceImpl.java
@@ -0,0 +1,30 @@
+package sample.service;
+
+import org.springframework.stereotype.Service;
+ 
+
+import java.util.List;
+import java.util.Map;
+ 
+
+import javax.annotation.Resource;
+ 
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import sample.dao.SampleDAO;
+ 
+@Service("sampleService")
+public class SampleServiceImpl implements SampleService{
+    Logger log = Logger.getLogger(this.getClass());
+     
+    @Resource(name="sampleDAO")
+    private SampleDAO sampleDAO;
+     
+    @Override
+    public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception {
+        return sampleDAO.selectBoardList(map);
+    }
+ 
+}
